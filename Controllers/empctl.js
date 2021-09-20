5330912fe4b39b16fb97a639a51357f56a306081
Kalyani <kalyanireddy9@gmail.com>
2021-09-19 20:20:22 -0700

    new changes

--- Controllers/empctl.js
+++ Controllers/empctl.js
@@ -5,8 +5,38 @@ const employee = require('../Model/emp');
 exports.create = ((req, res)=>{
     console.log("Create Employee Data")
     const {emp_id, emp_name, designation} = req.body;
-    employee.create({emp_id, emp_name, designation})
-    .then((empdetails)=> 
-    {res.status(200).send(empdetails)})
+    employee.create({emp_id, emp_name, designation})               // insert data in to data base using create
+    .then((empdetails)=>{res.status(200).send(empdetails)})
     .catch((err)=>{res.status(400).send(err)})
+
 }) 
+
+exports.list = ((req, res)=>{
+    console.log("Get All Employee Details");
+    employee.find({})                                               // read the recoed fform the data base using find method
+    .then((empdetails)=> {res.status(200).send(empdetails)})
+    .catch((err)=>{res.status(400).send(err)})
+
+
+})
+
+exports.update = ((req, res)=>{
+    console.log("Record To Be updated In Employee Details " + req.params);
+    console.log(req.params)
+    console.log(req.body)
+    employee.updateOne({emp_id:req.params.emp_id}, {designation:req.body.designation}) // update the recored in the data base using updateone method
+    .then((empdetails)=>{res.status(200).send(empdetails)})
+    .catch((err)=>{res.status(400).send(err)})
+  
+  })
+      
+
+exports.delete = ((req, res)=>{
+  console.log("Record To Be Deleted From Employee Details");
+  employee.deleteOne({emp_id:req.params.emp_id})                       // delete the recored from the data base
+  .then((empdetails)=>{res.status(200).send(empdetails)})
+  .catch((err)=>{res.status(400).send(err)})
+
+})
+    
+    
--- Views/router.js
+++ Views/router.js
@@ -2,8 +2,10 @@
 
 const express = require('express');                   //Include express
 const router = express.Router();                      //express is having router method and assigning to router variable
-const empctl = require('../Controllers/empctl');
-console.log("Creating router")
-router.post('/api/emp/create', empctl.create)         //Based on this routung we will perfoem CRUD operations so we are importing the controller/empctl logic
-//router.route('/api/emp/create').post(empctl.create);     //Using router variable with route method we are defining the API's here. POST method we are creating the contoller   
+const empctl = require('../Controllers/empctl');      //Based on this routung we will perfoem CRUD operations so we are importing the controller/empctl logic
+router.post('/api/emp/create', empctl.create);         //Using router variable with route method we are defining the API's here. POST method we are inserting the data  
+router.get('/api/emp/details', empctl.list);           //To het the employee detals we are writing GET Operation. next go to empcontrl
+router.put('/api/emp/update/:emp_id', empctl.update);  // update the data using PUT Operation
+router.delete('/api/emp/delete/:emp_id', empctl.delete);// delete the record using DELETE Operation
+    
 module.exports = router;                              // to exposrt router to other modules
